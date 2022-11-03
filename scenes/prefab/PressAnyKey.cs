f6b09101abee2046a5d700744a0f2d9ee95d9e9a
Alexander Rothman <gnomesort@megate.ch>
2022-11-03 11:43:18 -0700

    Update PressAnyKey.
    Moves PressAnyKey.tscn to scenes/prefab because it has broader use
    outside of the Menu.
    
    Adds a toggle to PressAnyKey to control fading/blinking animation (only
    read at _Ready currently).
    
    Changes behavior on _UnhandledInput so that button release events and
    repeat (echo) events are not considered. This prevents the Pressed
    signal from firing multiple times in a row.

--- scenes/prefab/PressAnyKey.cs
+++ scenes/prefab/PressAnyKey.cs
@@ -1,16 +1,23 @@
 using Godot;
 
 public class PressAnyKey : Label {
+  [Export]
+  public bool Blinking { get; set; } = true;
+
   [Signal]
   public delegate void Pressed();
 
   public override void _Ready() {
     var animations = GetNode<AnimationPlayer>("Animations");
-    animations.Play("Blink");
+    if (Blinking)
+    {
+      animations.Play("Blink");
+    }
   }
 
   public override void _UnhandledInput(InputEvent ev) {
-    if (ev is InputEventKey || ev is InputEventMouseButton || ev is InputEventJoypadButton)
+    if ((ev is InputEventKey || ev is InputEventMouseButton || ev is InputEventJoypadButton) && ev.IsPressed()
+        && !ev.IsEcho())
     {
       GetTree().SetInputAsHandled();
       EmitSignal("Pressed");
