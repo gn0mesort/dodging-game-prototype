b2c8ac20f77edb7b7274ce2f703c02bafc07235d
Alexander Rothman <gnomesort@megate.ch>
2022-11-04 12:19:17 -0700

    Re #1: Remove redundant menu buttons.
    Merges the start game and continue buttons into a single contextually
    initialized button.
    
    Disables the rebirth button if the player is already in the initial
    progress state (i.e., Player.Progress is 0).
    
    Exposes the PlayerData store as Player in GameMain.

--- scenes/menu/SecondaryTitle.cs
+++ scenes/menu/SecondaryTitle.cs
@@ -9,17 +9,33 @@ public class SecondaryTitle : VBoxContainer {
   [Signal]
   public delegate void TransitionRoot(RootScenes to);
 
+  private GameMain _main = null;
 
   private void _OnExitPressed() {
     EmitSignal("TransitionRoot", RootScenes.Exit);
   }
 
-  private void _OnStartGamePressed() {
+  private void _OnPlayPressed() {
     EmitSignal("TransitionRoot", RootScenes.Play);
   }
 
+  public override void _EnterTree() {
+    _main = GetNode<GameMain>("/root/Main");
+  }
+
   public override void _Ready() {
-    GetNode("StartGame").Connect("pressed", this, "_OnStartGamePressed");
+    var playButton = GetNode<Button>("Play");
+    var rebirthButton = GetNode<Button>("Rebirth");
+    if (_main.Player.Progress == 0)
+    {
+      playButton.Text = "New Game";
+      rebirthButton.Disabled = true;
+    }
+    else
+    {
+      playButton.Text = "Continue";
+    }
+    playButton.Connect("pressed", this, "_OnPlayPressed");
     GetNode("Exit").Connect("pressed", this, "_OnExitPressed");
   }
 
