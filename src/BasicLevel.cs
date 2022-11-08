b37cc809ed26bdcd188a4e5d7e57fcfeb933e6f5
Alexander Rothman <gnomesort@megate.ch>
2022-11-08 11:37:15 -0800

    Add Level base types.
    This adds a new abstract Level class which enforces the basic properties
    all game levels must have. Currently, a game level must be a Spatial
    (3D) node and expose an entrance and exit position (both as Vector3).
    
    A basic level implementation is also provided. Ideally, this will be
    sufficient for all game levels but other (more complex) levels could
    exist too.

---
+++ src/BasicLevel.cs
@@ -0,0 +1,25 @@
+using Godot;
+
+public class BasicLevel : Level {
+  private Position3D _entrance = null;
+  private Position3D _exit = null;
+
+  [Export]
+  public NodePath EntrancePosition { get; set; } = null;
+
+  [Export]
+  public NodePath ExitPosition { get; set; } = null;
+
+  public override Vector3 Entrance() {
+    return _entrance.Translation;
+  }
+
+  public override Vector3 Exit() {
+    return _exit.Translation;
+  }
+
+  public override void _Ready() {
+    _entrance = GetNode<Position3D>(EntrancePosition);
+    _exit = GetNode<Position3D>(ExitPosition);
+  }
+}
