4dd9374286cad7a559a1b6fe4e9cf203bcf05b66
Alexander Rothman <gnomesort@megate.ch>
2022-11-23 11:53:04 -0800

    Add license statements to source files.

--- src/IVelocityModifiable.cs
+++ src/IVelocityModifiable.cs
@@ -1,4 +1,21 @@
 /**
+ * Copyright (C) 2022 Alexander Rothman <gnomesort@megate.ch>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
  * @brief An interface representing objects that can have their velocity modified by in game effects.
  *
  * This should be applied to objects that are affected by the Field entity.
